7c6b17099f091935b4d5508cd25763ff78db15cf
BeiZhu9527 <2951407800@qq.com>
2024-05-18 17:06:34 +0800

    init

---
+++ pages/first/first.js
@@ -0,0 +1,71 @@
+// pages/first.js
+Page({ 
+  /**
+   * 页面的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function(options) {
+    // 使用 setTimeout 设置延时跳转
+    setTimeout(function() {
+      // 跳转到下一个页面
+      wx.redirectTo({
+        url: '/pages/index/index'
+      });
+    }, 2000); // 设置延时时间为2秒，单位为毫秒
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload() {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh() {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom() {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage() {
+
+  }
+})
